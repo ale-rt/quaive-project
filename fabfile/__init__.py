9532bc122cfd67b643522593c9bc90e23f5a17c3
ale-rt <alessandro.pisa@gmail.com>
2019-05-06 11:25:10 +0200

    Added a fabric task to run the robot test

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -2,14 +2,21 @@
 from fabric import api
 
 
+def robottest(name):
+    """ Run the robot test matching "name"
+    """
+    api.local(
+        "./bin/robot -t '{}' src/ploneintranet/src/ploneintranet/suite/tests/acceptance/".format(  # noqa: E501
+            name
+        )
+    )
+
+
 def robotserver():
     """ Run the ploneintranet robot server
     """
     with api.lcd("~/Code/plone/projects/quaive"):
         with api.prefix(". bin/activate"):
-            print(
-                "./bin/robot -t 'Test name' src/ploneintranet/src/ploneintranet/suite/tests/acceptance/"  # noqa: E501
-            )
             api.local(
                 "DIAZO_ALWAYS_CACHE_RULES=1 ./bin/robot-server -dvvv -l ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT"  # noqa: E501
             )
