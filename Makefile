98d7630c6edbcc5107d34845bd983052442d2e2c
ale-rt <alessandro.pisa@gmail.com>
2019-11-08 18:02:42 +0100

    First try with Plone5.2

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all checkout-ploneintranet
+.PHONY: all
 
 all: .installed.cfg
 
@@ -8,11 +8,9 @@ bin/buildout: bin/pip2.7 src/ploneintranet/requirements.txt
 bin/pip2.7:
 	virtualenv -p python2.7 --no-site-packages .
 
-checkout-ploneintranet:
+src/ploneintranet:
 	test -x src/ploneintranet || git clone git@github.com:quaive/ploneintranet.git src/ploneintranet
-	cd src/ploneintranet && git checkout master && git pull origin master && touch -mt `git log -1 --date=format:%Y%m%d%H%M.%S --pretty=format:%ad .` .
-
-src/ploneintranet: checkout-ploneintranet
+	cd src/ploneintranet && git checkout plone5.2 && git pull origin plone52 && touch -mt `git log -1 --date=format:%Y%m%d%H%M.%S --pretty=format:%ad .` .
 
 src/ploneintranet/requirements.txt: src/ploneintranet
 
--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -6,7 +6,17 @@ def robottest(name):
     """ Run the robot test matching "name"
     """
     api.local(
-        "./bin/robot --variable BROWSER:chrome -t '{}' src/ploneintranet/src/ploneintranet/suite/tests/acceptance/".format(  # noqa: E501
+        "ZSERVER_HOST=127.0.0.2 ZSERVER_PORT=55001 ./bin/robot --variable BROWSER:chrome -t '{}' src/ploneintranet/src/ploneintranet/suite/tests/acceptance/".format(  # noqa: E501
+            name
+        )
+    )
+
+
+def robottestff(name):
+    """ Run the robot test matching "name"
+    """
+    api.local(
+        "ZSERVER_HOST=127.0.0.2 ZSERVER_PORT=55001 ./bin/robot -t '{}' src/ploneintranet/src/ploneintranet/suite/tests/acceptance/".format(  # noqa: E501
             name
         )
     )
@@ -18,5 +28,5 @@ def robotserver():
     with api.lcd("~/Code/plone/projects/quaive"):
         with api.prefix(". bin/activate"):
             api.local(
-                "DIAZO_ALWAYS_CACHE_RULES=1 ./bin/robot-server -dvvv -l ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT"  # noqa: E501
+                "ZSERVER_HOST=127.0.0.2 ZSERVER_PORT=55001 DIAZO_ALWAYS_CACHE_RULES=1 ./bin/robot-server -dvvv -l ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT"  # noqa: E501
             )
