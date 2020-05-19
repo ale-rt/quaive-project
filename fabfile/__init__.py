8aa90402bae105bcd65f27ce5792d66d965020bd
ale-rt <alessandro.pisa@gmail.com>
2020-05-19 18:07:21 +0200

    Fabric 2

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,31 +1,31 @@
 # coding=utf-8
-from fabric import api
+from invoke import task
 
-
-def robottest(name):
+@task
+def robottest(c, name):
     """ Run the robot test matching "name"
     """
-    api.local(
+    c.run(
         "ZSERVER_HOST=127.0.0.2 ZSERVER_PORT=55001 ./bin/robot --variable BROWSER:chrome -t '{}' src/ploneintranet/src/ploneintranet/suite/tests/acceptance/".format(  # noqa: E501
             name
         )
     )
 
-
-def robottestff(name):
+@task
+def robottestff(c, name):
     """ Run the robot test matching "name"
     """
-    api.local(
+    c.run(
         "ZSERVER_HOST=127.0.0.2 ZSERVER_PORT=55001 ./bin/robot -t '{}' src/ploneintranet/src/ploneintranet/suite/tests/acceptance/".format(  # noqa: E501
             name
         )
     )
 
-
-def robotserver():
+@task
+def robotserver(c):
     """ Run the ploneintranet robot server
     """
-    with api.prefix(". bin/activate"):
-        api.local(
+    with c.prefix(". bin/activate"):
+        c.run(
             "ZSERVER_HOST=127.0.0.2 ZSERVER_PORT=55001 DIAZO_ALWAYS_CACHE_RULES=1 ./bin/robot-server -dvvv -l ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT ploneintranet.suite.testing.PLONEINTRANET_SUITE_ROBOT"  # noqa: E501
         )
