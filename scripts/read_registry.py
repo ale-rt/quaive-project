15d9ef453bd44e38192435c06c6d4b80e1b01cfe
ale-rt <alessandro.pisa@gmail.com>
2025-04-18 12:52:13 +0200

    Fix read registry to handle event subscribers

--- scripts/read_registry.py
+++ scripts/read_registry.py
@@ -1,6 +1,7 @@
 from plone.app.registry.browser.records import FakeEnv
 from plone.app.registry.exportimport.handler import RegistryImporter
 from transaction import commit
+from zope.component.hooks import setSite
 
 import sys
 
@@ -9,6 +10,7 @@ sites = app.objectValues("Plone Site")  # noqa:F821
 
 for filename in sys.argv[3:]:
     for site in sites:
+        setSite(site)
         importer = RegistryImporter(site.portal_registry, FakeEnv())
         with open(filename) as f:
             print(f"Importing {filename} in {site}")
